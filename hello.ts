d8bd81f1e0f33244867993dc222e96df32bca277
Youngzx <1216238955@qq.com>
2022-12-05 22:41:08 +0800

    看完字面量与枚举

--- hello.ts
+++ hello.ts
@@ -23,3 +23,30 @@ let joo:Joo = {
 declare let foo2:Foo;
 declare let joo2:Joo;
 // console.log(foo2 = joo2)
+
+interface obj1 {
+  name: string,
+  age:number
+}
+
+type obj2 = {
+  name: string,
+  age:number
+}
+
+var youngzx:obj1 = {
+  name:"Youngzx",
+  age:12,
+};
+
+console.log(youngzx)
+
+var yuwant:obj2 = {
+  name:'yuwant',
+  age:18
+}
+console.log(yuwant)
+
+interface Tmp {
+  mixed: true | string | 599 | {} | (() => {}) | (1 | 2)
+}
